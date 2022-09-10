4b9e02f2825ae4fc6406fd7b032cfdfd89190e7a
Emma Vatine <emma@loftorbital.com>
2022-09-09 17:17:03 -0700

    feat: Added Timeline component to packages/vis.

---
+++ packages/vis/src/components/Timeline.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue-demi';
+
+import type { PropType } from 'vue-demi';
+
+import type {
+  DataItem,
+  DataGroup,
+  DataGroupCollectionType,
+  TimelineEvents,
+  TimelineOptions,
+} from 'vis-timeline/esnext';
+
+import { useTimeline } from '../composables/useTimeline';
+
+const visualization = ref<HTMLElement>();
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<DataItem[]>,
+    required: true,
+  },
+  groups: {
+    type: Array as PropType<DataGroup[]>,
+    required: false,
+    default: () => {
+      return [] as DataGroupCollectionType;
+    },
+  },
+  currentTime: {
+    type: Date as PropType<Date>,
+    required: false,
+    default: () => {
+      return new Date();
+    },
+  },
+  options: {
+    type: Object as PropType<TimelineOptions>,
+    required: false,
+    default: () => {
+      return {} as TimelineOptions;
+    },
+  },
+  events: {
+    type: Array as PropType<TimelineEvents[]>,
+    required: false,
+    default: () => [
+      'click',
+      'contextmenu',
+      'currentTimeTick',
+      'doubleClick',
+      'drop',
+      'mouseOver',
+      'mouseDown',
+      'mouseUp',
+      'mouseMove',
+      'groupDragged',
+      'changed',
+      'rangechange',
+      'rangechanged',
+      'select',
+      'itemover',
+      'itemout',
+      'timechange',
+      'timechanged',
+    ],
+  },
+});
+const emit = defineEmits<{
+  (e: TimelineEvents, value: any): void;
+}>();
+const {
+  timeline,
+  selection,
+  setSelection,
+  items,
+  setItems,
+  groups,
+  setGroups,
+  options,
+  setOptions,
+  addCustomTime,
+  fit,
+  focus,
+  getCurrentTime,
+  getCustomTime,
+  getEventProperties,
+  getItemRange,
+  getSelection,
+  getVisibleItems,
+  getWindow,
+  moveTo,
+  redraw,
+  removeCustomTime,
+  setCurrentTime,
+  setCustomTime,
+  setCustomTimeTitle,
+} = useTimeline(visualization, {
+  items: props.items,
+  groups: props.groups,
+  options: props.options,
+  currentTime: ref(props.currentTime),
+});
+onMounted(() => {
+  if (!timeline.value) return;
+  props.events.forEach((event) => {
+    timeline.value?.on(event, (props) => emit(event, props));
+  });
+});
+</script>
+
+<template>
+  <div ref="visualization"></div>
+</template>
