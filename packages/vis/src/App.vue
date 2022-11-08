465717d102e3a8b5e46f1b1c7b270c7fc13ea0d8
Michael J. Roberts <michael@observerly.com>
2022-11-08 11:09:51 +0000

    chore: Added examples of Timeline usage to app.
    chore: Added examples of Timeline usage to app.

--- packages/vis/src/App.vue
+++ packages/vis/src/App.vue
@@ -1,8 +1,69 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { Timeline } from './components'
+
+import {
+  DataSet
+} from 'vis-data'
+
+import {
+  TimelineEvents,
+  TimelineOptions,
+} from 'vis-timeline/esnext';
+
+const items = [
+  {
+    id: 2,
+    group: 0,
+    content: "item 2",
+    start: "2014-04-14"
+  },
+  {
+    id: 3,
+    group: 0,
+    content: "item 3",
+    start: "2014-04-18"
+  },
+  {
+    id: 1,
+    group: 0,
+    content: "item 1",
+    start: "2014-04-20"
+  },
+  {
+    id: 4,
+    group: 0,
+    content: "item 4",
+    start: "2014-04-16",
+    end: "2014-04-19"
+  },
+  {
+    id: 5,
+    group: 0,
+    content: "item 5",
+    start: "2014-04-25"
+  },
+  {
+    id: 6,
+    group: 0,
+    content: "item 6",
+    start: "2014-04-27",
+    type: "point"
+  }
+]
+
+const options = {} as TimelineOptions;
+
+const handleClick = (event: TimelineEvents) => {
+  console.log(event)
+}
 </script>
 
 <template>
-
+  <Timeline 
+    :items="items"
+    :options="options"
+    @click="handleClick"
+  />
 </template>
--- packages/vis/src/main.ts
+++ packages/vis/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "vis-timeline/styles/vis-timeline-graph2d.css";
+
 createApp(App).mount('#app')
